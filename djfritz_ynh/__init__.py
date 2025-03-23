b30ad800988d2a44ee943a0f558cebb4b17b44d1
Jens Diemer <git@jensdiemer.de>
2025-03-23 18:56:52 +0100

    Apply manageprojects updates

--- djfritz_ynh/__init__.py
+++ djfritz_ynh/__init__.py
@@ -1,8 +1,8 @@
 """
-    djfritz_ynh
-    Web based FritzBox management using Python/Django.
+djfritz_ynh
+Web based FritzBox management using Python/Django.
 """
 
 # See https://packaging.python.org/en/latest/specifications/version-specifiers/
-__version__ = '0.4.0+ynh1'
+__version__ = '0.4.0+ynh2'
 __author__ = 'Jens Diemer <git@jensdiemer.de>'
